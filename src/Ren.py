ea349041f25d0ad875c17a395a4b7f2614e922c7
Ren6525 <hunterren652@gmail.com>
2023-02-13 13:35:07 -0700

    hj

---
+++ src/Ren.py
@@ -0,0 +1,12 @@
+def find_Ball():
+    Vis.take_snapshot(ball)
+    object = Vis.largest_object()
+    if object:
+        distance = get_dist(object.centerY, object.width)
+        differenceOfX = object.centerX - expectedX
+        return(distance, differenceOfX)
+    else:
+        return false
+
+def turn_to_ball:
+    
